811602e789aa6d86e1c111255d196ee563bda759
Ozioma EMEREM <code.emerem@gmail.com>
2023-10-19 17:33:03 +0100

    stack

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - prints the top
+ * @front: stack front
+ * @num_popped: line_number
+ * Return: no return
+*/
+void f_stack(stack_t **front, unsigned int num_popped)
+{
+	(void)front;
+	(void)num_popped;
+	custom_struct.stack_queue_flag = 0;
+}
