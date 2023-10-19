4d42bba3df302a66a93fbf69e41147784e4e9359
EMarvelM <marvelousegbe19@gmail.com>
2023-10-19 17:38:55 +0100

    nop

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- nothing
+  *@front: stack front
+  *@num_popped: line_number
+  *Return: no return
+ */
+void f_nop(stack_t **front, unsigned int num_popped)
+{
+	(void) num_popped;
+	(void) front;
+}
